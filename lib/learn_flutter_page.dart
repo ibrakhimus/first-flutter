8df891f93d2a39f51f147d02f7b844006f460c4a
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-03-15 21:33:14 -0400

    f

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -1,3 +1,123 @@
 import 'package:flutter/material.dart';
 
 // https://www.youtube.com/watch?v=C-fKAzdTrLU&t=1171s
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckbox = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Learn Flutter'),
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                debugPrint('Actions');
+              },
+              icon: const Icon(
+                Icons.info_outline,
+              ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('images/einstein.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                color: Colors.black,
+              ),
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.blueGrey,
+                width: double.infinity,
+                child: const Center(
+                  child: Text(
+                    'yoooo',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isSwitch ? Colors.amberAccent : Colors.blue,
+                ),
+                onPressed: () {
+                  debugPrint('Elevated btn');
+                },
+                child: const Text('Elevated btn'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  debugPrint('Outlined btn');
+                },
+                child: const Text('Outlined btn'),
+              ),
+              TextButton(
+                onPressed: () {
+                  debugPrint('Text btn');
+                },
+                child: const Text('Text btn'),
+              ),
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  debugPrint('this is the row');
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Icon(
+                      Icons.local_fire_department,
+                      color: Colors.blue,
+                    ),
+                    Text('Row widget'),
+                    Icon(
+                      Icons.local_fire_department,
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),
+              Switch(
+                  value: isSwitch,
+                  onChanged: (bool newBool) {
+                    setState(() {
+                      isSwitch = newBool;
+                    });
+                  }),
+              Checkbox(
+                  value: isCheckbox,
+                  onChanged: (bool? newBool) {
+                    setState(() {
+                      isCheckbox = newBool;
+                    });
+                  }),
+              Image.network(
+                  'https://i2-prod.mirror.co.uk/incoming/article25029327.ece/ALTERNATES/s338a/0_talismantate_60066237_138974360513703_1095349483887891174_n.jpg')
+            ],
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,13 +28,17 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter"),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('Floating Action Button');
